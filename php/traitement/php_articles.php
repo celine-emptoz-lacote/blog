0ce845c79f0cdf17edc8e5c2f0a2798965eed966
celine-emptoz-lacote <62133744+celine-emptoz-lacote@users.noreply.github.com>
2020-07-13 17:16:38 +0200

    MODIFS

--- php/traitement/php_articles.php
+++ php/traitement/php_articles.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php                   
     $par_page = 5;    
              
@@ -30,38 +29,6 @@
             //Récupère tous les articles liés à la catégorie, limiter à 5 par page à partir du 0 (puis 5, 10...)
             $query_categorie_articles = $bd->query("SELECT * FROM articles WHERE id_categorie=$get_id_categorie ORDER BY date DESC LIMIT $a_partir_du, $par_page");
             $categorie_articles = $query_categorie_articles->fetchAll(PDO::FETCH_ASSOC);        
-=======
-<?php
-       
-     require 'php/include/connexion.php';
-      
-
-    //Création de la pagination
-    //Compte le nombre d'articles
-    $query_count_articles = $bd->query("SELECT COUNT(id) as count_articles FROM articles");
-    $count_articles = $query_count_articles->fetch();     
-    
-    //Initialise les variables pour la pagination
-    $nb_articles = $count_articles['count_articles'];     
-    $par_page = 5;    
-    $nb_pages = ceil($nb_articles/$par_page);        
-    
-    //Regarde le numéro de la page
-    if(isset($_GET["p"]) && $_GET["p"]>0 && $_GET["p"]<=$nb_pages)
-        {
-            $page = (int) strip_tags($_GET["p"]);
-        }
-    else
-        {
-            $page = 1;
-        }       
-
-    $a_partir_du = (($page-1)*$par_page); //Permet de savoir à partir de quel article on commence l'affichage
-    
-    //Récupère tous les articles limiter à 5 par page à partir du 0 (puis 5, 10...)
-    $query_all_articles = $bd->query("SELECT * FROM articles ORDER BY date DESC LIMIT $a_partir_du, $par_page");
-    $all_articles = $query_all_articles->fetchAll(PDO::FETCH_ASSOC);    
->>>>>>> celine
 
             //Récupère le titre de la catégorie
             $query_titre_cat = $bd->query("SELECT nom FROM categories WHERE id=$get_id_categorie");
